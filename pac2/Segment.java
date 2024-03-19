7ff07527c45aeda49e7d245a5d43dca5deb63f19
jmalvarez <jmalvarez@uma.es>
2024-03-19 11:30:51 +0100

    New version of Segment.java

--- pac2/Segment.java
+++ pac2/Segment.java
@@ -1,15 +1,15 @@
 package pac2;
 
-//import pac1.Point; // Option with import
+import pac1.Point; // Option with import
 
 public class Segment {
 
-	private pac1.Point origin, end; // Option without import
-//    private Point origin, end;  // Option with import
+//	private pac1.Point origin, end; // Option without import
+    private Point origin, end;  // Option with import
 	
 	public Segment(double x1, double y1, double x2, double y2) {
-		origin = new pac1.Point(x1, y1);
-		end = new pac1.Point(x2, y2);
+		origin = new Point(x1, y1);
+		end = new Point(x2, y2);
 	}
 	
 	// Rest of methods

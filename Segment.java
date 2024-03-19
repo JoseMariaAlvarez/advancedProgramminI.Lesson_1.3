f46c2b1c59ef7f370cf91338fb9b9de1e0189064
jmalvarez <jmalvarez@uma.es>
2024-03-19 11:28:04 +0100

    New version Segment.java

--- Segment.java
+++ Segment.java
@@ -1,22 +1,27 @@
 public class Segment {
 
-  private Point origin, end;
+	private Point origin, end;
 
-//  public Segment(Point pnt1, Point pnt2) { 
-//    origin = pnt1;
-//    end = pnt2;
-//  }
+	//  public Segment(Point pnt1, Point pnt2) { 
+	//    origin = pnt1;
+	//    end = pnt2;
+	//  }
 
-public Segment(Point pnt1, Point pnt2) { 
-    origin = new Point(pnt1.abscissa(), 
-    		           pnt1.ordinate());
-    end = new Point(pnt2.abscissa(), 
-    		        pnt2.ordinate());
-  }
+	public Segment(Point pnt1, Point pnt2) { 
+		origin = new Point(pnt1.abscissa(), 
+				pnt1.ordinate());
+		end = new Point(pnt2.abscissa(), 
+				pnt2.ordinate());
+	}
 
-  // Other methods ...
+	// Other methods ...
 
-  public double length() {
-    return origin.distance(end);
-  }
+	public void move (double a, double b) {
+		origin.move(a,  b);
+		end.move(a, b);
+	}
+
+	public double length() {
+		return origin.distance(end);
+	}
 }
